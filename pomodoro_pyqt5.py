eee7db8166655699e5c30d730222bc52066740d3
wang <ywang0789@conestogac.on.ca>
2023-10-19 03:45:47 -0400

    replace tkinker with QPushButtons

--- pomodoro_pyqt5.py
+++ pomodoro_pyqt5.py
@@ -18,6 +18,34 @@ class PomodoroTimer(QWidget):
         super().__init__()
 
         self.setWindowTitle("make_me_productive.exe")
+        self.workTime = 20 * 5  # work time
+        self.breakTime = 5 * 60  # break time
+        self.isBreak = False
+        self.isRunning = False
+        self.timeRemaining = self.workTime
+
+        # Layout and widgets
+        layout = QVBoxLayout()
+        self.label = QLabel(self.formatTime(self.timeRemaining))
+        self.label.setFont(QFont("Arial", 48))
+        layout.addWidget(self.label)
+
+        self.start_button = QPushButton("▶️")
+        self.start_button.setFont(QFont("Segoe UI Emoji", 12))
+        self.start_button.clicked.connect(self.start)
+        layout.addWidget(self.start_button)
+
+        self.stop_button = QPushButton("⏸️")
+        self.stop_button.setFont(QFont("Segoe UI Emoji", 12))
+        self.stop_button.clicked.connect(self.stop)
+        layout.addWidget(self.stop_button)
+
+        self.quit_button = QPushButton("❌")
+        self.quit_button.setFont(QFont("Segoe UI Emoji", 12))
+        self.quit_button.clicked.connect(self.close)
+        layout.addWidget(self.quit_button)
+
+        self.setLayout(layout)
 
 
 if __name__ == "__main__":
